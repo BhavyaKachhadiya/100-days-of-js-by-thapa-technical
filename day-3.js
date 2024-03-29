95b2fb79f4f174a3ab023e970897027de88ed27b
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2024-03-29 18:15:17 +0530

    add : day- 3 doc

--- day-3.js
+++ day-3.js
@@ -1,3 +1,13 @@
+// Day -3
+
+/**
+ * The function `countChar` takes a string and a character as input, and returns the count of how many
+ * times that character appears in the string.
+ * @param str - "This is function will count the character"
+ * @param char - c
+ * @returns The function `countChar` is returning the count of the character "c" in the given string
+ * "This is function will count the character".
+ */
 const countChar = (str,char) => {
     const words = str.toLowerCase().split("");
     let count=0;
